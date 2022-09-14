c27547e28e8b19a7166740deef6a579a222be58f
jacca25 <annejacca@gmail.com>
2022-09-14 13:31:24 -0700

    hey

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 	{
 	while (minutes <= 59)
 	{
-
+	mins_remainder - minutes % 10;
     	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');
@@ -28,4 +28,5 @@ void jack_bauer(void)
 	}
 	hours++;
 	minutes = 0;
+}
 }
