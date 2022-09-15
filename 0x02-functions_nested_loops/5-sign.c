7c19e8df3b0d983724a49d01404c4b22b5855e8a
jacca25 <annejacca@gmail.com>
2022-09-14 19:44:48 -0700

    hey

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,6 @@
  *
  * Return: 1 if positive, 0 if zero, -1 if negative
  */
-
 int print_sign(int n)
 {
 if (n > 0)
@@ -13,13 +12,11 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-
 else if (n == 0)
 {
 _putchar('0');
 return (0);
 }
-
 else
 {
 _putchar('-');
