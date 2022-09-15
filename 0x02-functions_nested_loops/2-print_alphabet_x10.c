252f8be5b7a0c1fa54c7d43403e2018a0d75bd09
jacca25 <annejacca@gmail.com>
2022-09-15 08:48:55 -0700

    hey

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#nclude "main.h"
 
 /**
- * print_alphabet_x10 - print the alphabet in lowercase.
- * followed by a new line
- * Return: 0 (success)
- */
+* print_alphabet_x10 - print the alphabet in lowercase.
+* followed by a new line
+* Return: 0 (success)
+*/
 
 void print_alphabet_x10(void)
 {
@@ -21,4 +21,5 @@ letter++;
 _putchar('\n');
 letter = 'a';
 number++;
-} 
+}
+}
