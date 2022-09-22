15c060371545074109ef36053c6e442c277c513f
jacca25 <annejacca@gmail.com>
2022-09-22 13:04:59 -0700

    hey

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+You're running out of storage (99%). … Soon you won't be able to upload new files to Drive and send or receive emails in Gmail.Learn more
+
+#include "main.h"
+
+/**
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+int index = 0, dest_len = 0;
+
+while (dest[index++])
+dest_len++;
+
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
+
+return (dest);
+}
